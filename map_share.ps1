706287be622fb9233d583481d964c47d295d1c5f
edx <edward.x.howard@veeam.com>
2020-05-06 10:20:52 +0100

    first commit

---
+++ map_share.ps1
@@ -0,0 +1,37 @@
+﻿Add-PSSnapin VeeamPSSnapin
+
+# Select the text file with the shares listed in \\host\share format
+$filePath = Read-Host -Prompt "Enter hostfile path: "
+
+# Select the repository
+$repository = Get-VBRBackupRepository | Out-GridView -Title "Cache Repository selection" -PassThru
+
+# Select the proxy
+$proxy = Get-VBRNASProxyServer | Out-GridView -Title "File proxies" -PassThru
+
+# Select the backup processing mode
+$mode = Read-Host -Prompt "Enter processing mode: VSSSnapshot or Direct (VSS Recommended): "
+
+# Set credentials for the share
+$credentials = Get-Credential -Message "Share Credentials"
+
+foreach($share in Get-Content -Path $filePath) {
+
+try {
+
+Add-VBRNASSMBServer -Path $share -AccessCredentials $credentials -SelectedProxyServer $proxy -ProxyMode SelectedProxy -ProcessingMode $mode -CacheRepository $repository -ErrorAction Stop -ErrorVariable $err
+
+} catch {
+
+ $errormessage = $_.Exception.Message 
+ $errormessage 
+
+}
+
+}
+
+
+
+
+
+
